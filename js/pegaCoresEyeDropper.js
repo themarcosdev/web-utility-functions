c5b48e555b0acbe69101b1ca94432cc74f4aea9d
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-01-16 08:14:20 -0300

    Update pegaCoresEyeDropper.js
    Alterando validação para garantirmos a existência dos parâmetros, caso não informados assumem um valor padrão, validação para garantir que o browser possui suporte ao EyeDropper.

--- js/pegaCoresEyeDropper.js
+++ js/pegaCoresEyeDropper.js
@@ -16,57 +16,65 @@
 
         /* Validações */
 
-        /* Validando formaDeRetorno */
-        if(!opcoesDeRetorno.toLocaleLowerCase().includes(formaDeRetorno)){
-            formaDeRetorno = null;
-        }
-
+        /* Validando existência */
         if(!formaDeRetorno){
             formaDeRetorno = 'console';
-        }
-
-        /* Validando copiarParaTransferencia */
-        if(!opcoesDeCopia.toLocaleLowerCase().includes(copiarParaTransferencia)){
-            copiarParaTransferencia = null;
         }
 
         if(!copiarParaTransferencia){
             copiarParaTransferencia = 'n';
         }
 
+        /* Validando formaDeRetorno */
+        formaDeRetorno = formaDeRetorno.toLowerCase();
+        if(!opcoesDeRetorno.includes(formaDeRetorno)){
+            formaDeRetorno = 'console';
+        }
+
+        /* Validando copiarParaTransferencia */
+        formaDeRetorno = formaDeRetorno.toLowerCase();
+        if(!opcoesDeCopia.includes(copiarParaTransferencia)){
+            copiarParaTransferencia = 'n';
+        }
+
         /* Criando o EyerDropper */
-        let eyeDropper = new EyeDropper();
+        if (typeof EyeDropper == 'function'){
+            let eyeDropper = new EyeDropper();
 
-        alert(`Ao clicar na página será criado um conta-gotas para seleção de cores.\nAo clicar novamente o valor da cor que está focada será exibido através do ${formaDeRetorno}`);
-        
-        let body = document.body
-        
-        /* Criando subfunção para inputar uma string na área de transferência de copia|cola */
-        const copyToClipboard = string => {
-            if (navigator && navigator.clipboard && navigator.clipboard.writeText){
-                return navigator.clipboard.writeText(string);
-            } else {
-                return Promise.reject('Erro : Clipboard API não está disponível .');
-            }
-        };
-        
-        /* Criando listerner para toda vez que clicar no body da página exibir o conta gotas e retornar a cor em hex */
-        body.addEventListener('click', e => {
-            eyeDropper.open().then(colorSelectionResult => {
-                /* Tratamento para forma de retorno */
-                if(formaDeRetorno == 'console'){
-                    console.log(colorSelectionResult.sRGBHex);
-                } else if (formaDeRetorno == 'alerta'){
-                    alert(colorSelectionResult.sRGBHex);
+            alert(`Ao clicar na página será criado um conta-gotas para seleção de cores.\nAo clicar novamente o valor da cor que está focada será exibido através do ${formaDeRetorno}`);
+            
+            let body = document.body
+            
+            /* Criando subfunção para inputar uma string na área de transferência de copia|cola */
+            const copyToClipboard = string => {
+                if (navigator && navigator.clipboard && navigator.clipboard.writeText){
+                    return navigator.clipboard.writeText(string);
+                } else {
+                    return Promise.reject('Erro : Clipboard API não está disponível .');
                 }
+            };
+            
+            /* Criando listerner para toda vez que clicar no body da página exibir o conta gotas e retornar a cor em hex */
+            body.addEventListener('click', e => {
+                eyeDropper.open().then(colorSelectionResult => {
+                    /* Tratamento para forma de retorno */
+                    if(formaDeRetorno == 'console'){
+                        console.log(colorSelectionResult.sRGBHex);
+                    } else if (formaDeRetorno == 'alerta'){
+                        alert(colorSelectionResult.sRGBHex);
+                    }
 
-                /* Tratamento para copiar para área de transferência */
-                if(copiarParaTransferencia == 's'){
-                    copyToClipboard(`${colorSelectionResult.sRGBHex}`);
-                }
-            })
-            .catch(error => {
-                console.log(error);
+                    /* Tratamento para copiar para área de transferência */
+                    if(copiarParaTransferencia == 's'){
+                        copyToClipboard(`${colorSelectionResult.sRGBHex}`);
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
             });
-        });
-    };
+        } else {
+            alert('Erro : Função EyeDropper não encontrada no navegador');
+            return false;
+        }
+    }
