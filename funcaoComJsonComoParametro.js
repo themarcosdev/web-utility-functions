3924c32f0b35fbc833bf41d6bf31dc90a7aa5de6
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-01-21 21:56:42 -0300

    Create funcaoComJsonComoParametro.js
    Esta função recebe um JSON o e usa o itens do JSON como parâmetro para função .

---
+++ funcaoComJsonComoParametro.js
@@ -0,0 +1,29 @@
+  /**
+    Esta função recebe um JSON o e usa o itens do json como parâmetro ;
+    Traz a vantagem de informar um parâmetro independente da ordem informada ;
+    @example :
+     * let json = {param1:'abc',param2:'123'}  ;
+     * let json2 = {param2:'123',param1:'abc'} ;
+     
+     * recebeJson(json) ;
+     * recebeJson(json2) ;
+  */
+  function recebeJson(paramsViaJson){
+        if(!paramsViaJson){
+            return 'informe os parâmetros para essa função';
+        }
+
+        params = paramsViaJson;
+
+        if(!params.param1){
+            return 'informe o parâmetro param1';
+        }
+
+        if(!params.param2){
+            return 'informe o parâmetro param2';
+        }
+        
+        return `Esta função recebeu o parametro 1 : ${params.param1}
+                e o parametro 2 : ${params.param2}
+        `
+    }
