e9500f27cf6e0fb7ea6297009ae561aade5b0292
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-02-03 07:57:31 -0300

    Corrigindo função.

--- js/limitaArray.js
+++ js/limitaArray.js
@@ -1,19 +1,32 @@
-function limitaArray(array,novoItem,tamMaxArray){
-    if(!array){
-        return 'informe o array';
-    }
-    if(!novoItem){
-        return 'informe o novo item';
-    }
-    if(!tamMaxArray){
-        return 'informe o tamanho máx. do array';
+    let meuArray = [10,20,30];
+
+    /** 
+        função para acrescentar um novo item a um array e limita-lo a um tamanho máximo ;
+        @example : 
+        limitaArray(meuArray,40,3) ;
+        limitaArray(meuArray,50,3) ;
+        limitaArray(meuArray,60,3) ;
+    */
+    function limitaArray(array,itemNovoNoArray,tamMaxArray){
+        if(!array){
+            return `Informe um array `;
+        }
+
+        if(!itemNovoNoArray){
+            return `Informe um novo item para o array `;
+        }
+
+        if(!tamMaxArray){
+            return `Informe o tamanho máximo no array `;
+        }
+
+        /* Adicionado novo item ao final do array */
+        array.push(itemNovoNoArray) ;
+        /* Removendo o primeiro item do array para respeitar o tamMaxArray */
+        if(array.length > tamMaxArray ){
+            array = array.shift();
+        }
+
+        return array ;
     }
 
-    array = array.push(novoItem);
-    if(array.length > tamMaxArray){
-        array.shift();
-    }
-
-    return array;
-
-}
