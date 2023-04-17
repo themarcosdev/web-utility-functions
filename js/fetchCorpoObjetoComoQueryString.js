b7c741abd56e863aab50be59b454dc67e2b53317
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-04-17 15:45:29 -0300

    Transf.  Objeto de dados legível em um querystring

---
+++ js/fetchCorpoObjetoComoQueryString.js
@@ -0,0 +1,28 @@
+function FetchURLSearch(){
+	let meus_headers = {
+		'Content-Type': 'application/json'
+	}
+  
+	/* Transformando objeto de dados legível em um querystring */
+	let dados = 	{
+		param_1: "1",
+		d1: 1 ,
+		d2: 2 ,
+		d3: null,
+	}
+	let url = new URLSearchParams(dados);
+	url = url.toString();
+
+  // Site de exemplo ;
+	fetch(`https://example.com?${url}`, {
+	method: 'GET',
+	headers: meus_headers
+	}).then(response => {
+		//
+		console.log(response);
+		response.json().then(function (data) {
+			console.log(data);
+		});
+	});
+
+}
