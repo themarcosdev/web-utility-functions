9fa3f2c61bbb2d59a3d253f359e6c5f59f81622f
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-06-22 17:25:16 -0300

    Create ajaxSetupHeadersDebug.js

---
+++ js/ajaxSetupHeadersDebug.js
@@ -0,0 +1,58 @@
+$.ajaxSetup({
+    beforeSend: function (jqXHR, settings) {
+        let retorno = {};
+        let requisicao = {};
+        let invocador = null;
+
+        /* Inicializador da função ; */
+        try {
+            throw new Error();
+        } catch (e) {
+            const stack = e.stack.split('\n');
+            const caller = stack[stack.length - 1];
+            invocador = caller.trim();
+        }
+
+        /* Intercepta a requisição aqui (Dados Enviados) ; */
+        requisicao.url = settings.url;
+        requisicao.type = settings.type;
+        requisicao.data = settings.data;
+        requisicao.headers = settings.headers;
+
+        retorno.invocador = invocador;
+        retorno.requisicao = requisicao;
+
+        /* Guardando valores de requisição para $ajax.setup complete  ;*/
+        sessionStorage.setItem('sst_retorno_ir', JSON.stringify(retorno));
+    },
+    complete: function (jqXHR, textStatus) {
+        let retorno = {};
+        let resposta = {};
+
+        /* Clonar response, para que a original possa ler os dados e usar o clone para a let retorno ; */
+        let clone = jqXHR.responseText;
+
+        /* Tratamento na devolução de retorno.resposta ; */
+        resposta = {
+            body: clone,
+            headers: jqXHR.getAllResponseHeaders(),
+            status: jqXHR.status,
+            statusText: jqXHR.statusText
+        };
+
+        if (resposta.body) {
+            resposta.body = resposta.body.replace(/[\t\r\n]/g, "");
+            if (resposta.body.charAt(0) == "[" || resposta.body.charAt(0) == "{") {
+                resposta.body = JSON.parse(resposta.body);
+            }
+        }
+
+        retorno.resposta = resposta;
+        retorno.requisicao = JSON.parse(sessionStorage.getItem('sst_retorno_ir')).requisicao;
+        retorno.invocador = JSON.parse(sessionStorage.getItem('sst_retorno_ir')).invocador;
+
+        /* Faça o que quiser com o retorno | Debug ; */
+        console.log(retorno);
+        sessionStorage.setItem('sst_retorno_as', JSON.stringify(retorno));
+    }
+});
