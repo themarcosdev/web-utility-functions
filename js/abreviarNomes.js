43466e57b9b618caa4cae8ed824ae4e6f41756f2
Douglas Vicentini Ferreira <dvferreira@bartofil.com.br>
2023-08-31 11:26:57 -0300

    Melhorando mensagem de retorno e substituindo 'split' for 'charAt'.

--- js/abreviarNomes.js
+++ js/abreviarNomes.js
@@ -1,77 +1,70 @@
-	/**
-	 * Função para abreviar um nome
-	 * @author Marcos T. de L. Coelho ;
-	 * @param {*} nome nome a ser abreviado ; 
-	 * @param {*} tamanhoMax  tamanho máximo que é esperado que o nome seja abreviado ;
-	 * @param {*} reducaoAoTamMax parâmetro para forçar um nome a ser abreviado (S | N) ;
-	 * @returns 
-	 */
-	function abreviarNomes(nome, tamanhoMax, reducaoAoTamMax) {
-		/* Declarações e validações */
-		let nomeAbreviado = null;
-		let nomesAbreviados = [];
-		let nomes = [];
+/**
+ * Função para abreviar um nome
+ * @author Marcos T. de L. Coelho ;
+ * @param {*} nome nome a ser abreviado ;
+ * @param {*} tamanhoMax  tamanho máximo que é esperado que o nome seja abreviado ;
+ * @param {*} reducaoAoTamMax parâmetro para forçar um nome a ser abreviado (S | N) ;
+ * @see https://github.com/themarcosdev/funcoes-utilidades-web/blob/main/js/abreviarNomes.js
+ * @returns
+ */
+function abreviarNomes(nome, tamanhoMax, reducaoAoTamMax) {
+    /* Declarações e validações */
+    let nomeAbreviado   = null;
+    let nomesAbreviados = [];
+    let nomes           = [];
 
-		if (!nome) {
-			return 'Informe um nome !';
-		}
+    if (!nome) {
+        return 'Informe um nome !';
+    }
+    if (!tamanhoMax) {
+        tamanhoMax = 30;
+    }
+    if (!reducaoAoTamMax || reducaoAoTamMax != "S") {
+        reducaoAoTamMax = 'N';
+    }
 
-		if (!tamanhoMax) {
-			tamanhoMax = 30;
-		}
+    nome = nome.trim();
+    nome = nome.toLocaleUpperCase();
 
-		if (!reducaoAoTamMax || reducaoAoTamMax != "S") {
-			reducaoAoTamMax = 'N';
-		}
+    /* Abreviando o nome, pode ocorrer do nome e sobrenome serem muito grandes e passarem o tamanhoMax*/
+    if (nome.length >= tamanhoMax) {
+        let posNome                  = 0;
+        nomes                        = nome.split(' ');
+        nomeAbreviado                = nomes[0];
+        let nomesAbreviadosIgnorados = ["DA", "DAS", "DE", "DO", "DOS", "E", "&"];
+        let totalNomes               = nomes.length - 1;
+        for (nome of nomes) {
+            if (posNome > 0 && posNome < totalNomes) {
+                if (!nomesAbreviadosIgnorados.includes(nome)) {
+                    if (!(+nome.match(/\d+/g))) {
+                        nomeAbreviado += ' ' + nome.charAt(0) + '.';
+                    }
+                }
+            }
+            posNome++;
+        }
+        nomeAbreviado += ' ' + nomes[totalNomes];
+    }
 
-		nome = nome.trim();
+    /* Forçando redução do nome ao tamanho solicitado */
+    if (nomeAbreviado && reducaoAoTamMax == 'S') {
+        nomesAbreviados = nomeAbreviado.split(' ');
+        let tnomesabrev = nomesAbreviados.length;
 
-		nome = nome.toLocaleUpperCase();
+        for (let i = 0; i < tamanhoMax; i++) {
+            if (i > 0 && i < tnomesabrev) {
+                if (nomeAbreviado.length > tamanhoMax) {
+                    nomeAbreviado = nomeAbreviado.replace(new RegExp(nomesAbreviados[i], 'g'), '');
+                }
+            }
+        }
+        nomeAbreviado = nomeAbreviado.replace(/\s+/g, ' ').trim();
+    }
 
-		/* Abreviando o nome, pode ocorrer do nome e sobrenome serem muito grandes e passarem o tamanhoMax*/
-		if (nome.length >= tamanhoMax) {
-			let posNome = 0;
-			nomes = nome.split(' ');
-			nomeAbreviado = nomes[0];
-			let nomesAbreviadosIgnorados = ["DA", "DAS", "DE","DO", "DOS", "E", "&"];
-			let totalNomes = nomes.length - 1;
-			for (nome of nomes) {
-				if (posNome > 0 && posNome < totalNomes) {
-					if (!nomesAbreviadosIgnorados.includes(nome)) {
-						if (!(+nome.match(/\d+/g))) {
-							nomeAbreviado += ' ' + nome.split('')[0] + '.';
-						}
-					}
-				}
-
-				posNome++;
-			}
-
-			nomeAbreviado += ' ' + nomes[totalNomes];
-
-		}
-
-		/* Forçando redução do nome ao tamanho solicitado */
-		if (nomeAbreviado && reducaoAoTamMax == 'S') {
-			nomesAbreviados = nomeAbreviado.split(' ');
-			let tnomesabrev = nomesAbreviados.length;
-
-			for (let i = 0; i < tamanhoMax; i++) {
-				if (i > 0 && i < tnomesabrev) {
-					if (nomeAbreviado.length > tamanhoMax) {
-						nomeAbreviado = nomeAbreviado.replace(new RegExp(nomesAbreviados[i], 'g'), '');
-					}
-				}
-			}
-
-			nomeAbreviado = nomeAbreviado.replace(/\s+/g, ' ').trim();
-
-		}
-
-		/* Retornando nome abreviado ou falha */
-		if (nomeAbreviado && reducaoAoTamMax && nomeAbreviado.length > tamanhoMax && reducaoAoTamMax == 'S') {
-			return 'Falha: não foi possível reduzir o nome';
-		} else {
-			return nomeAbreviado ?? nome ;
-		}
-	}
+    let nomeFinal = nomeAbreviado ?? nome;
+    if (nomeAbreviado && reducaoAoTamMax && nomeAbreviado.length > tamanhoMax && reducaoAoTamMax == 'S') {
+        return nomeFinal.substring(0, tamanhoMax);
+    } else {
+        return nomeFinal;
+    }
+}
