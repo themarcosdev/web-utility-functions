5807c065e1907795c39af0397533e591e4f833d7
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-11-16 15:43:18 -0300

    Create changeElemPosition.js

---
+++ js/changeElemPosition.js
@@ -0,0 +1,13 @@
+function changeElemPosition(elem1, elem2) {
+    if (!elem1 || !elem2 || elem1.tagName != elem2.tagName) {
+        return false;
+    }
+
+    properties1 = elem1.innerHTML;
+    properties2 = elem2.innerHTML;
+
+    elem2.innerHTML = properties1;
+    elem1.innerHTML = properties2;
+
+    console.log('Ok');
+}
