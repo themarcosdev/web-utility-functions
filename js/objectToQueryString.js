b8087da3b5a4ee61ae067b3d536eb2b3978960d9
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-12-15 13:57:44 -0300

    Create objectToQueryString.js

---
+++ js/objectToQueryString.js
@@ -0,0 +1,14 @@
+function objectToQueryString(obj) {
+    let queryString = '';
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+        if (queryString !== '') {
+            queryString += '&';
+        }
+        queryString += `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
+        }
+    }
+
+    return `?${queryString}`;
+}
