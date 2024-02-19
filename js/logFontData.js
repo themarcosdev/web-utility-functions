fbadbf788011aec1ab66a9001b3950a24e7c96b5
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2024-02-19 16:36:26 -0300

    Create logFontData.js

---
+++ js/logFontData.js
@@ -0,0 +1,20 @@
+async function logFontData() {
+	try {
+		let res = [];
+		const availableFonts = await window.queryLocalFonts();
+		for (const fontData of availableFonts) {
+			res.push(
+				{
+				postscriptName : fontData.postscriptName,
+				fullName       : fontData.fullName,
+				family         : fontData.family,
+				script         : fontData.script
+				}
+			);
+		}
+
+		return res;
+	} catch (err) {
+		console.error(err.name, err.message);
+	}
+}
