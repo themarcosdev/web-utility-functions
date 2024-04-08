b74a6c34d1f6fb38b0fa5a45923ad3244feb4ae6
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2024-04-08 15:08:34 -0300

    Update StringPackFunctions.js

--- js/StringPackFunctions.js
+++ js/StringPackFunctions.js
@@ -3,9 +3,17 @@
 */
 
 /**
+   example of use :
+  'abc'.capitalize(); // 'Abc';
+*/
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+/**
   example of use :
-  'abc'.isBiggerThan(1); // true ;
-  'abc'.isBiggerThan(5); // false ;
+  'abc'.isBiggerThan(1); // true;
+  'abc'.isBiggerThan(5); // false;
 */
 String.prototype.isBiggerThan = function(size) {
     if (this.length > size) {
