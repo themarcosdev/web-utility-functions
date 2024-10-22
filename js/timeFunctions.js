a0066a03436c2279c4bc779f17907649fb3087b3
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2024-10-22 14:02:36 -0300

    Update timeFunctions.js

--- js/timeFunctions.js
+++ js/timeFunctions.js
@@ -14,3 +14,33 @@ window.formatTime = function(numberInSeconds, formatTo) {
         return 'Formato inválido. Escolha entre: minutes, hours, days.';
     }
 }
+
+/**
+* @example: parseTime('01:01:02:01'); // 90121 | DD:HH:MM:SS
+*/
+window.parseTime = function(timeString) {
+    let timeParts = timeString.split(':').map(part => parseInt(part, 10)); // Converter todas as partes para números
+
+    let totalSeconds = 0;
+
+    if (timeParts.length === 2) {
+        // Formato MM:SS
+        let [minutes, seconds] = timeParts;
+        totalSeconds = (minutes * 60) + seconds;
+
+    } else if (timeParts.length === 3) {
+        // Formato HH:MM:SS
+        let [hours, minutes, seconds] = timeParts;
+        totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+
+    } else if (timeParts.length === 4) {
+        // Formato DD:HH:MM:SS
+        let [days, hours, minutes, seconds] = timeParts;
+        totalSeconds = (days * 86400) + (hours * 3600) + (minutes * 60) + seconds;
+
+    } else {
+        throw new Error("Formato inválido. Use MM:SS, HH:MM:SS ou DD:HH:MM:SS");
+    }
+
+    return totalSeconds;
+};
